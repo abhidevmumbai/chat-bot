c6b7b61499cdec5a5ba6bcf252f647cb4ea7bc77
abhishek <abhidevmumbai@gmail.com>
2021-04-19 23:16:45 -0400

    Added movie api endpoint

--- services/http.ts
+++ services/http.ts
@@ -1,25 +1,53 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 class HttpService {
-    constructor() {}
+    private weatherAPI;
+    private movieApi;
+
+    constructor() {
+        this.weatherAPI = {
+            url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+            key: '17e5d1c9a4msh9626219b27e0824p1a0965jsn0f56fbb373c6',
+            host: 'community-open-weather-map.p.rapidapi.com',
+        };
+        this.movieApi = {
+            url: 'https://api.themoviedb.org',
+            key: 'd27343ea256fcb2b0be1c09ae5363957',
+            language: 'en-US',
+        };
+    }
 
     async getWeatherByLocation(location) {
         const options: AxiosRequestConfig = {
             method: 'GET',
-            url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+            url: this.weatherAPI.url,
             params: {
                 q: location,
                 units: 'metric',
             },
             headers: {
-                'x-rapidapi-key':
-                    '17e5d1c9a4msh9626219b27e0824p1a0965jsn0f56fbb373c6',
-                'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+                'x-rapidapi-key': this.weatherAPI.key,
+                'x-rapidapi-host': this.weatherAPI.host,
             },
         };
 
         const response = await axios.request(options);
         return response.data.main.temp;
+    }
+
+    async getMovieGenres() {
+        const options: AxiosRequestConfig = {
+            method: 'GET',
+            url: `${this.movieApi.url}/3/genre/movie/list`,
+            params: {
+                api_key: this.movieApi.key,
+                language: this.movieApi.language,
+            },
+        };
+
+        const response = await axios.request(options);
+        let genres = response.data.genres.map((item) => item.name);
+        return genres.join(', ');
     }
 }
 
